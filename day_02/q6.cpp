d44fc78fd6e2b39f4c4b15e6a16004c160cce7b5
Vivek <25110cn481@glbitm.ac.in>
2026-05-30 19:31:51 +0530

    day_02 uploaded

---
+++ day_02/q6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,rev=0,rem;
+    cout << "enter the number ";
+    cin >> n;
+    while(n!=0){
+        rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    cout << "the reverse of number " << rev;
+    return 0;
+}
