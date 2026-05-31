af12f56b069851c14ca871721f9d896b20d2de9d
Vivek <25110cn481@glbitm.ac.in>
2026-05-31 19:48:54 +0530

    day_09 uploaded

---
+++ day_09/q34.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout << "enter the number of rows ";
+    cin >> n;
+    for(i=n;i>=1;i--){
+        for(j=1;j<=i;j++){
+            cout <<j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ day_09/q36.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout << "enter the number of rows ";
+    cin >> n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(i==1||i==n||j==1||j==n){
+                cout <<"*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
